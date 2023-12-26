b6f96c6fa8baa497a6b71f248722f4bcae837146
Roman <bezuglijj7@gmail.com>
2023-12-27 00:29:45 +0200

    added footer

--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -1,6 +1,7 @@
 const menu = document.querySelector(".menu");
 const menuOpen = document.querySelector(".header__burger");
 const menuClose = document.querySelector(".menu__close");
+const menuItems = document.querySelectorAll(".menu__item a");
 const body = document.querySelector("body");
 
 menuOpen.addEventListener("click", () => {
@@ -11,4 +12,12 @@ menuOpen.addEventListener("click", () => {
 menuClose.addEventListener("click", () => {
   menu.classList.remove("open");
   body.classList.remove("no-scroll");
+});
+
+// Close the menu when a menu item is clicked
+menuItems.forEach((menuItem) => {
+  menuItem.addEventListener("click", () => {
+    menu.classList.remove("open");
+    body.classList.remove("no-scroll");
+  });
 });
